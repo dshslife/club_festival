6aa9aa2b4f863a0389171313d6821b16bd89c398
happycastle <41810556+happycastle114@users.noreply.github.com>
2021-07-12 15:32:52 +0900

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from djongo import models
-
+from django import forms
 # Create your models here.
 
 class Booth(models.Model):
@@ -9,3 +9,22 @@ class Booth(models.Model):
     message = models.TextField()
     code = models.TextField()
     busy = models.IntegerField()
+
+    objects = models.DjongoManager()
+
+class BoothLog(models.Model):
+    name = models.TextField()
+    code = models.TextField()
+    date = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+class BoothLogForm(forms.ModelForm)
+
+class User(models.Model):
+    _id = models.ObjectIdField()
+    award = models.BooleanField()
+    bingo = models.ArrayField()
+
+    objedts = models.DjongoManager()
